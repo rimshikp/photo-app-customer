355be89c6e67ea70d2e28c3524f3f9c2181a0432
msafvan <mohamed.safvan@3pillarglobal.com>
2025-05-29 22:32:02 +0530

    initial commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const backendUrl = "http://localhost:4000";
+export const backendUrl = "https://photo-app-backend-kbnj.onrender.com";
 export const razorPayKey = "rzp_test_W6FMLPfJeo1u3a";
 export const razorPaySecret = "kRx699VfTRmRoi06cmW5iaEY";
